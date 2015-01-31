2ab7048d97f7aaf8a6913502e4edefad315afbb0
axton21 <axton_n@yahoo.com>
2015-01-31 15:35:23 -0500

    labs took forever

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
+	# bucket = []
+	# array.each do |x|
+		if array.length == 1
+			array.join
+		elsif array. length == 2
+			array.join(' and ')
+		else edge = 'and '+array.last
+			array.pop
+			array<<edge
+			array.join(', ')
+		#else
+	end
+# end
+	#array.each {|x| print x + ", "}
+	#array.last {|y| print "and " + y}
 
 end
